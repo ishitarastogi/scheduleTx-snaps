898c5f8ad8efa096f9d708e707ac6ed0690b9197
Ishita Rastogi <46647968+ishitarastogi@users.noreply.github.com>
2023-02-18 23:14:40 +0530

    Update index.ts

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -2,10 +2,7 @@ import { OnRpcRequestHandler } from '@metamask/snaps-types';
 import { OnCronjobHandler } from '@metamask/snaps-types';
 import { ethers } from 'ethers';
 
-
-
-
-  const sendTransaction = async () => {
+ const sendTransaction = async () => {
     // Initialize the Ethereum provider
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
@@ -35,23 +32,28 @@ import { ethers } from 'ethers';
       const response = await signer.sendTransaction(tx);
       console.log(response);
     };
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns `null` if the request succeeded.
- * @throws If the request method is not valid for this snap.
- * @throws If the `snap_confirm` call failed.
- */
 
-export const onRpcRequest: OnRpcRequestHandler = async ({
-  origin,
-  request,
-}) => {
-{
+export const onCronjob = async ({ request }) => {
+//   const date = new Date(timestamp);
+//   const currentMinute = date.getMinutes();
+//   const currentHour = date.getHours();
+//   const currentDayOfMonth = date.getDate();
+//   const currentMonth = date.getMonth() + 1; // Date.getMonth() returns 0-11, so we add 1 to get 1-12
+//   const currentDayOfWeek = date.getDay();
+
+  const { minute, hour, dayOfMonth, month, dayOfWeek} = request.params;
+
+//currentMinute and all will accept inputs from users  
+  // Check if the current time matches the user's input
+  if (
+    ( minute === currentMinute) &&
+    ( hour === currentHour) &&
+    ( dayOfMonth === currentDayOfMonth) &&
+    (month === currentMonth) &&
+    (dayOfWeek === currentDayOfWeek)
+  ) {
+    // Run the custom logic
+    switch (request.method) {
     try {
       // Run the custom logic
       switch (request.method) {
